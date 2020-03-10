a21aaa790af543f585e42371922ad8b54d2a5c07
Ivan Sotelo <isotelo@controlla.com.mx>
2020-03-10 13:43:59 -0700

    fix: change theme

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -17,16 +17,16 @@ export default {
   },
   change (mode) {
     if (mode === 'darken') {
-      document.body.style.setProperty(`--pr-theme-bg`, '#f4f7f8')
-      document.body.style.setProperty(`--pr-theme-color`, '#2c3e50')
-      document.body.style.setProperty(`--pr-theme-layout`, '#fff')
-      document.body.style.setProperty(`--pr-theme-bg2`, '#eef2f5')
-      localStorage.setItem('theme', 'darker')
-    } else {
       document.body.style.setProperty(`--pr-theme-bg`, '#18191c')
       document.body.style.setProperty(`--pr-theme-color`, '#fff')
       document.body.style.setProperty(`--pr-theme-layout`, '#1e2023')
       document.body.style.setProperty(`--pr-theme-bg2`, '#141417')
+      localStorage.setItem('theme', 'darker')
+    } else {
+      document.body.style.setProperty(`--pr-theme-bg`, '#f4f7f8')
+      document.body.style.setProperty(`--pr-theme-color`, '#2c3e50')
+      document.body.style.setProperty(`--pr-theme-layout`, '#fff')
+      document.body.style.setProperty(`--pr-theme-bg2`, '#eef2f5')
       localStorage.setItem('theme', 'lighthen')
     }
   }
